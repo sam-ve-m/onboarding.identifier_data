e7f81fcd76df98318a69667c1bb2d05e92100227
Samuel Veloso Marçal <svm@lionx.com.br>
2022-09-08 13:18:03 -0300

    refact: cpf validation method

--- func/src/domain/validators/user_identifier_data.py
+++ func/src/domain/validators/user_identifier_data.py
@@ -16,28 +16,21 @@ class Cpf(BaseModel):
 
     @validator("cpf", always=True, allow_reuse=True)
     def validate_cpf(cls, cpf: str) -> str:
-        cpf_last_digits = cpf[:-2]
-        cont_reversed = 10
-        total = 0
+        if len(cpf) != 11:
+            raise IndexError("invalid cpf")
 
-        for index in range(19):
-            if index > 8:
-                index -= 9
-            total += int(cpf_last_digits[index]) * cont_reversed
-            cont_reversed -= 1
-
-            if cont_reversed < 2:
-                cont_reversed = 11
-                digits = 11 - (total % 11)
-
-                if digits > 9:
-                    digits = 0
-                total = 0
-                cpf_last_digits += str(digits)
-
-        sequence = cpf_last_digits == str(cpf_last_digits[0]) * len(cpf)
-        if not cpf == cpf_last_digits or sequence:
+        first_digit_validation = sum(int(cpf[index]) * (10-index) for index in range(9))
+        mod_first_digit = first_digit_validation % 11
+        first_digit = 11-mod_first_digit if mod_first_digit > 1 else 0
+        if str(first_digit) != cpf[-2]:
             raise ValueError("invalid cpf")
+
+        second_digit_validation = first_digit_validation + sum(map(int, cpf[:9])) + 2*first_digit
+        mod_second_digit = second_digit_validation % 11
+        second_digit = 11-mod_second_digit if mod_second_digit > 1 else 0
+        if str(second_digit) != cpf[-1]:
+            raise ValueError(f"invalid cpf")
+
         return cpf
 
 
@@ -68,5 +61,5 @@ class IdentifierData(Cpf, CelPhone):
 
 class UserIdentifier(BaseModel):
     user_identifier: IdentifierData
-    tax_residences: List[TaxResidence]
+    tax_residences: List[TaxResidence] = []
     us_person: Optional[bool]
