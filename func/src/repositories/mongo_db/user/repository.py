899951c4da2e8c9dc1bd4ca2d247397ed81c7ca6
Samuel Veloso Marçal <svm@lionx.com.br>
2022-10-06 09:23:02 -0300

    feat: verify if cpf is in blocklist

--- func/src/repositories/mongo_db/user/repository.py
+++ func/src/repositories/mongo_db/user/repository.py
@@ -1,46 +1,34 @@
-# Jormungandr - Onboarding
-from ..base_repository.base import MongoDbBaseRepository
+from decouple import config
 
-# Third party
-from etria_logger import Gladsheim
+from ..base_repository.base import MongoDbBaseRepository
 
 
 class UserRepository(MongoDbBaseRepository):
+
+    @staticmethod
+    def __set_collection(mongo_client):
+        database = mongo_client[config("MONGODB_DATABASE_NAME")]
+        collection = database[config("MONGODB_USER_COLLECTION")]
+        return collection
+
     @classmethod
     async def find_one_by_cpf(cls, cpf: str) -> dict:
-        collection = await cls._get_collection()
-        try:
-            identifier_data_document = await collection.find_one(
-                {"identifier_document.cpf": cpf}
-            )
-            return identifier_data_document
-        except Exception as ex:
-            message = f'UserRepository::find_one_by_email::with this query::"cpf":{cpf}'
-            Gladsheim.error(error=ex, message=message)
-            raise ex
+        identifier_data_document = await cls.find_one(
+            {"identifier_document.cpf": cpf}
+        )
+        return identifier_data_document
 
     @classmethod
     async def find_one_by_unique_id(cls, unique_id: str) -> dict:
-        collection = await cls._get_collection()
-        try:
-            user = await collection.find_one({"unique_id": unique_id})
-            return user
-        except Exception as ex:
-            message = f'UserRepository::find_one_user::with this query::"unique_id":{unique_id}'
-            Gladsheim.error(error=ex, message=message)
-            raise ex
+        user = await cls.find_one({"unique_id": unique_id})
+        return user
 
     @classmethod
     async def update_one_with_user_identifier_data(
         cls, unique_id: str, user_identifier_template: dict
     ):
-        collection = await cls._get_collection()
-        try:
+        with cls._get_collection() as collection:
             user_updated = await collection.update_one(
                 {"unique_id": unique_id}, {"$set": user_identifier_template}
             )
             return user_updated
-        except Exception as ex:
-            message = f'UserRepository::update_one_with_user_identifier_data::error on update identifier data":{user_identifier_template}'
-            Gladsheim.error(error=ex, message=message)
-            raise ex
--- func/src/services/user_identifier_data.py
+++ func/src/services/user_identifier_data.py
@@ -1,13 +1,16 @@
 # Jormungandr - Onboarding
+from datetime import datetime
+
 from ..domain.enums.types import UserOnboardingStep
 from ..domain.exceptions.exceptions import (
     CpfAlreadyExists,
     UserUniqueIdNotExists,
     ErrorOnUpdateUser,
-    InvalidOnboardingCurrentStep,
+    InvalidOnboardingCurrentStep, CpfBlocked,
 )
 from ..domain.identifier_data.model import UserIdentifierDataModel
 from ..domain.validators.user_identifier_data import UserIdentifier
+from ..repositories.mongo_db.blocklist.repository import BlockListRepository
 from ..repositories.mongo_db.user.repository import UserRepository
 from ..transports.audit.transport import Audit
 from ..transports.caf.transport import BureauApiTransport
@@ -24,7 +27,7 @@ class ServiceUserIdentifierData:
     async def validate_current_onboarding_step(jwt: str):
         user_current_step = await OnboardingSteps.get_user_current_step(jwt=jwt)
         if not user_current_step == UserOnboardingStep.IDENTIFIER_DATA:
-            raise InvalidOnboardingCurrentStep
+            raise InvalidOnboardingCurrentStep()
 
     async def register_identifier_data(self) -> bool:
         await Audit.record_message_log(self.user_identifier)
@@ -36,16 +39,24 @@ class ServiceUserIdentifierData:
             user_identifier_template=user_identifier_template,
         )
         if not user_updated.matched_count:
-            raise ErrorOnUpdateUser
+            raise ErrorOnUpdateUser()
         await BureauApiTransport.create_transaction(self.user_identifier)
         return True
 
     async def verify_cpf_and_unique_id_exists(self):
         result = await UserRepository.find_one_by_cpf(cpf=self.user_identifier.cpf)
         if result:
-            raise CpfAlreadyExists
+            raise CpfAlreadyExists()
         user = await UserRepository.find_one_by_unique_id(
             unique_id=self.user_identifier.unique_id
         )
         if not user:
-            raise UserUniqueIdNotExists
+            raise UserUniqueIdNotExists()
+
+    async def verify_cpf_is_in_blocklist(self):
+        is_defaulter = await BlockListRepository.is_cpf_in_block_list(
+            verification_data=datetime.utcnow().timestamp(),
+            cpf=self.user_identifier.cpf,
+        )
+        if is_defaulter:
+            raise CpfBlocked()
